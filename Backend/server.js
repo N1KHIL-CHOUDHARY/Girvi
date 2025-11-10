5a142a01749bdefed0e7b6f4cb220dec39af2ce0
N1KHIL-CHOUDHARY <nikhil2k7h@gmail.com>
2025-11-10 20:41:40 +0530

    frontend url add cors fix

--- Backend/server.js
+++ Backend/server.js
@@ -11,10 +11,10 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-const corsOptions = {
-  origin: process.env.FRONTEND_URL||'http://localhost:5173' // Replace with your actual frontend domain
-};
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+})); 
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
